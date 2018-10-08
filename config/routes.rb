e409e88a4555d5834f11487790b27e84d1a9d7e5
tadahiokada <tadahi.okada@gmail.com>
2018-10-08 16:58:30 +0900

    firstcommit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
+  }
+  get 'home/top'
+  get 'home/index'
+
+
+  root to: 'home#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
